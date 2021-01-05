915fe4e6f287e674a135a18d6074acdf962e6aad
Ricardo Olarte <raop155@hotmail.com>
2021-01-05 17:07:28 -0500

    feature(Posts) Fetch user's posts in a FlatList

---
+++ components/PostItem.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+const PostItem = ({ title, onPress }) => {
+  return (
+    <TouchableOpacity onPress={onPress} style={styles.container}>
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+export default PostItem;
+
+const styles = StyleSheet.create({
+  container: {
+    height: 50,
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    borderBottomColor: '#ccc',
+    borderBottomWidth: 1,
+    width: '100%',
+  },
+  text: {
+    fontSize: 18,
+    textTransform: 'capitalize',
+  },
+});
--- screens/Posts.jsx
+++ screens/Posts.jsx
@@ -1,10 +1,47 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, Text, View, FlatList, ActivityIndicator } from 'react-native';
+import { PostItem } from '../components';
 
-const PostsScreen = () => {
+const PostsScreen = ({ navigation }) => {
+  const userId = navigation.getParam('userId');
+  const [loading, setLoading] = useState(true);
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+        const data = await response.json();
+        setPosts(data);
+        setLoading(false);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      {loading ? (
+        <View style={styles.center}>
+          <ActivityIndicator size='large' color='#00ff' />
+          <Text style={styles.loading}>Loading...</Text>
+        </View>
+      ) : (
+        <FlatList
+          style={styles.list}
+          data={posts.filter((post) => post.userId === userId)}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <PostItem
+              onPress={() => navigation.navigate('Detail', { userId: item.id })}
+              {...item}
+            />
+          )}
+        />
+      )}
     </View>
   );
 };
@@ -15,7 +52,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  center: {
+    flex: 1,
+    width: '100%',
+    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  loading: {
+    fontSize: 20,
+  },
+  list: {
+    width: '100%',
   },
 });
